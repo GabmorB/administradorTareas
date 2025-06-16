e3e009376637de304b11a22aac080955e734348a
GabrielMFCA <gabmorenobreton@comunidad.unam.mx>
2025-06-16 15:55:54 -0600

    Menu y funciones: verTareas y agregarTareas

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 "Archivo de administrador de tareas"
+
+tareas = []
+
+def mostrarMenu():
+
+    print("Menu del administrador")
+
+    print("1. Ver tareas\n" \
+    "2. Agregar nueva tarea\n" \
+    "3. Marcar tarea como completada\n" \
+    "4. Eliminar tarea\n" \
+    "5. Salir")
+
+def verTareas():
+    print(tareas)
+
+def agregarTareas():
+    nuevaTarea = input("Escriba la tarea\n")
+    tareas.append(nuevaTarea)
+
+respuesta = ""
+while respuesta != "5":
+
+    mostrarMenu()
+
+    respuesta = input("Seleccione la accion\n")
+    
+    if respuesta == "1":
+        verTareas()
+        
+    elif respuesta == "2":
+        agregarTareas()
+
+
+
+
+
+menu()
